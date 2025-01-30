f753cd539f91094c5b327cd14dbb4d9b62425b9d
@hexlet/cli <support@hexlet.io>
2025-01-30 22:05:59 +0300

    submit java-spring-boot-ru/first-start_30.01.25_19.48.27

--- java-spring-boot-ru/first-start_30.01.25_19.48.27/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/first-start_30.01.25_19.48.27/src/main/java/exercise/Application.java
@@ -6,5 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // BEGIN
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "true");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/about")
+    String home() {
+        return "Welcome to Hexlet!";
+    }
+}
 
 // END
